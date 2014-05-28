d9aa4c5c4b94ac773722ee287731970562980e45
Hakimo <thimodupre@hotmail.com>
2014-05-28 15:15:04 +0200

    Create ScrollBarColor.js

---
+++ ScrollBarColor.js
@@ -0,0 +1,15 @@
+<!-- Hakimo -->
+     <STYLE>BODY {
+     
+     //- Using a function pointer:
+document.getElementById("Click Me! :D").onclick = doFunction;
+
+SCROLLBAR-FACE-COLOR: red; SCROLLBAR-HIGHLIGHT-COLOR: gray; 
+
+SCROLLBAR-SHADOW-COLOR: black; SCROLLBAR-ARROW-COLOR: gray; 
+
+SCROLLBAR-TRACK-COLOR: black; SCROLLBAR-DARKSHADOW-COLOR: red
+}
+</STYLE>
+
+<font face="Tahoma"><a target="_blank" href="http://www.javascriptfreecode.com/"><span style="font-size: 8pt; text-decoration: none">JavaScript Free Code</span></a></font>
